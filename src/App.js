360179db593136d165446ee621c64a7794903d39
MikeGit <Mikhail1999Ulasevich@mail.ru>
2021-03-27 13:11:06 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Carousel from "./components/Carousel";
+import "./Styles.css";
+
+const App = () => {
+    return (
+        <Carousel></Carousel>
+    );
+};
+
+export default App;
---
+++ src/components/Carousel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Carousel = () => {
+    return (
+        <h2>Hello carousel</h2>
+    )
+}
+
+export default Carousel
